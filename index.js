52fc7ec8aa95ee694024e8995ad5b5b7649878c2
Newton Costa <newtongomez04@gmail.com>
2022-11-09 18:23:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-// Changes overhere
+// Just another comment
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
